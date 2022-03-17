01835699bbbb863b9fe576509fa2ba62663a3a81
rabbi50 <jasrabbi50@gmail.com>
2022-03-18 01:50:52 +0600

    add contact component

--- app/components/blog.js
+++ app/components/blog.js
@@ -2,28 +2,27 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import $ from 'jquery';
 export default class BlogComponent extends Component {
-    @action
-    blogCarousel(){
-
-        // Blog owlCarousel
+  @action
+  blogCarousel() {
+    // Blog owlCarousel
     $('.blog .owl-carousel').owlCarousel({
-        loop:true,
-        margin: 30,
-        mouseDrag:false,
-        autoplay:true,
-        smartSpeed:500,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            700:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
+      loop: true,
+      margin: 30,
+      mouseDrag: false,
+      autoplay: true,
+      smartSpeed: 500,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        700: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+      },
     });
-    }
+  }
 }
