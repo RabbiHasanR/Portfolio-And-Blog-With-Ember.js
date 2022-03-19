23857a320140bdaa75025c7d0bc169932f1ad19f
Rabbi <jasrabbi50@gmail.com>
2022-03-19 16:19:32 +0600

    blog slider issue fix

--- app/components/blog.js
+++ app/components/blog.js
@@ -4,25 +4,28 @@ import $ from 'jquery';
 export default class BlogComponent extends Component {
   @action
   blogCarousel() {
-    // Blog owlCarousel
-    $('.blog .owl-carousel').owlCarousel({
-      loop: true,
-      margin: 30,
-      mouseDrag: false,
-      autoplay: true,
-      smartSpeed: 500,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        700: {
-          items: 2,
-        },
-        1000: {
-          items: 3,
-        },
-      },
+
+    console.log('blogCarousel');
+
+    $('#carousel-multiple').on('slide.bs.carousel', function (e) {
+      var itemsPerSlide = parseInt(
+          $(this).attr('data-maximum-items-per-slide')
+        ),
+        totalItems = $('.carousel-item', this).length,
+        reserve = 1, //do not change
+        $itemsContainer = $('.carousel-inner', this),
+        it = itemsPerSlide + reserve - (totalItems - e.to);
+        console.log('slide items:',it, itemsPerSlide, totalItems, e.to);
+
+      if (it > 0) {
+        for (var i = 0; i < it; i++) {
+          console.log('e driection:', e.direction, $itemsContainer)
+          $('.carousel-item', this)
+            .eq(e.direction == 'left' ? i : 0)
+            // append slides to the end/beginning
+            .appendTo($itemsContainer);
+        }
+      }
     });
   }
 }
--- app/components/testimonial.js
+++ app/components/testimonial.js
@@ -3,16 +3,5 @@ import Component from '@glimmer/component';
 // import $ from 'jquery';
 
 export default class TestimonialComponent extends Component {
-  // @action
-  // testimonialCarousel() {
-  //   // Testimonials owlCarousel
-  //   $('.testimonials .owl-carousel').owlCarousel({
-  //     items: 1,
-  //     loop: true,
-  //     margin: 15,
-  //     mouseDrag: false,
-  //     autoplay: true,
-  //     smartSpeed: 500,
-  //   });
-  // }
+
 }
