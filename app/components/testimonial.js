088f6b25727e2d296cbaa9d0834c3a8230be0238
Rabbi <jasrabbi50@gmail.com>
2022-03-19 14:16:26 +0600

    testimonial slider fix

--- app/components/testimonial.js
+++ app/components/testimonial.js
@@ -1,18 +1,18 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import $ from 'jquery';
+// import { action } from '@ember/object';
+// import $ from 'jquery';
 
 export default class TestimonialComponent extends Component {
-  @action
-  testimonialCarousel() {
-    // Testimonials owlCarousel
-    $('.testimonials .owl-carousel').owlCarousel({
-      items: 1,
-      loop: true,
-      margin: 15,
-      mouseDrag: false,
-      autoplay: true,
-      smartSpeed: 500,
-    });
-  }
+  // @action
+  // testimonialCarousel() {
+  //   // Testimonials owlCarousel
+  //   $('.testimonials .owl-carousel').owlCarousel({
+  //     items: 1,
+  //     loop: true,
+  //     margin: 15,
+  //     mouseDrag: false,
+  //     autoplay: true,
+  //     smartSpeed: 500,
+  //   });
+  // }
 }
