6afb61960c1eb1c707aa711b218b597006ad5dea
Rabbi <jasrabbi50@gmail.com>
2022-03-25 01:54:50 +0600

    hire me and download cv icon fix

--- app/components/portfolio.js
+++ app/components/portfolio.js
@@ -3,30 +3,34 @@ import { action } from '@ember/object';
 import $ from 'jquery';
 
 export default class PortfolioComponent extends Component {
+
   @action
   portfolioGallery() {
     // magnificPopup
-    $('.gallery').magnificPopup({
-      delegate: '.link',
-      type: 'image',
-      gallery: {
-        enabled: true,
-      },
-    });
+    // $('.gallery').magnificPopup({
+    //   delegate: '.link',
+    //   type: 'image',
+    //   gallery: {
+    //     enabled: true,
+    //   },
+    // });
     // isotope
     $('.gallery').isotope({
       // options
       itemSelector: '.items',
+      layoutMode: 'fitRows',
     });
 
     var $gallery = $('.gallery').isotope({
       // options
     });
 
+    $gallery.isotope({filter: '*'});
+
     // filter items on button click
     $('.filtering').on('click', 'span', function () {
       var filterValue = $(this).attr('data-filter');
-
+      console.log('filtervalue:',filterValue);
       $gallery.isotope({ filter: filterValue });
     });
 
